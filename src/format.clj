a4e6bc7608514c4c8eea0cf9a1f4d7c9190b91e4
10plusY <grigsmusic1990@gmail.com>
2017-06-18 18:44:04 -0400

    spacing working

---
+++ src/format.clj
@@ -0,0 +1,17 @@
+ (ns teamz.format)
+
+(defn- generate-empty-char-row [char length]
+  "Generates a row of all one character"
+  (->> char
+       (repeat length)
+       (string/join "")))
+
+(defn format-for-display [team {:keys [spacing]}]
+  "Uses various formatting options to present the final string..."
+  (cond
+    (nil? spacing)
+    (interleave
+      team
+      (repeat
+        (count team)
+        (generate-empty-char-row "-" (count (first team)))))))
---
+++ src/out.clj
@@ -0,0 +1,49 @@
+ (ns teamz.out
+   (:require [clojure.string :as string]))
+
+(defn- pad-string [string pad-char]
+  "Fundamental operation: pad a string
+   with choice character..."
+  (str pad-char string pad-char))
+
+(defn- padded-enough? [width]
+  "Stupidly redundant way of asking if padded string
+   is equal to desired width or one off for odd spacing..."
+  (not
+    (nil? (some #(= width %) [0 1]))))
+
+(defn- longest-string-length [string-coll]
+  "Takes collection of strings and returns
+   length of longest string..."
+  (apply max (map #(count %) string-coll)))
+
+ ;; format
+
+(defn pad-string-to-length [length char string]
+  "Recursively pad a name with a separator character
+   until it is a desired length..."
+  (loop [padded string]
+    (let [pad-distance (- length (count padded))]
+      (if-not (padded-enough? pad-distance)
+        (recur (pad-string padded char))
+        padded))))
+
+(defn pad-all-strings-equally [string-collection]
+  "Takes collection of strings and pads appropriate length...
+  Returns a lazy seq..."
+  (map
+    (partial pad-string-to-length (longest-string-length string-collection) "-")
+    string-collection))
+
+(defn players->row [players]
+  "Takes players in a row and formats a string
+   so that they are adequetely spaced..."
+  (clojure.string/join "" (pad-all-strings-equally players)))
+
+ ;; TODO: still a seq
+
+(defn rows->team [rows]
+  "Formats the output representation of the team...
+   Assumes that rows are of varying lengths and need formatting..."
+  (map pad-all-strings-equally rows))
+
