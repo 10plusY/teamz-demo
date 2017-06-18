30f0c38dcf862c3d6895c1dae9dd342c94e6cb36
10plusY <grigsmusic1990@gmail.com>
2017-06-18 19:50:16 -0400

    remove private helpers

--- src/out.clj
+++ src/out.clj
@@ -1,36 +1,24 @@
 (ns teamz.out)
 
-(defn- pad-string [string pad-char]
-  "Fundamental operation: pad a string
-   with choice character..."
-  (str pad-char string pad-char))
-
-(defn- padded-enough? [width]
-  "Stupidly redundant way of asking if padded string
-   is equal to desired width or one off for odd spacing..."
-  (not
-    (nil? (some #(= width %) [0 1]))))
-
-(defn- longest-string-length [string-coll]
-  "Takes collection of strings and returns
-   length of longest string..."
-  (apply max (map #(count %) string-coll)))
-
 (defn pad-string-to-length [length char string]
   "Recursively pad a name with a separator character
    until it is a desired length..."
-  (loop [padded string]
-    (let [pad-distance (- length (count padded))]
-      (if-not (padded-enough? pad-distance)
-        (recur (pad-string padded char))
-        padded))))
+  (letfn [(pad-string [str ch] (str ch str ch))
+          (padded-enough? [dist] (<= 0 dist 2))]
+    (loop [padded string]
+      (let [pad-string (fn [str ch] (str ch str ch))
+            pad-distance (- length (count padded))]
+        (if-not (padded-enough? pad-distance)
+          (recur (pad-string padded char))
+          padded)))))
 
 (defn pad-all-strings-equally [string-collection]
   "Takes collection of strings and pads appropriate length...
   Returns a lazy seq..."
-  (map
-    (partial pad-string-to-length (longest-string-length string-collection) "-")
-    string-collection))
+  (letfn [(longest-string-length [col] (apply max (map #(count %) col)))]
+    (map
+      (partial pad-string-to-length (longest-string-length string-collection) "-")
+      string-collection)))
 
 (defn players->row [players]
   "Takes players in a row and formats a string
